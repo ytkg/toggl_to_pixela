e46b29b3b6042437ff23273ae1c9669a3676a372
Yoshiki Takagi <yoshiki.tkg@gmail.com>
2021-04-12 13:09:45 +0900

    [update] データがない場合は即終了

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ date = Date.today - 1
 
 toggl = Toggl.new(ENV['TOGGL_API_TOKEN'])
 summary = toggl.summary('toggl_to_pixela', ENV['TOGGL_WORKSPACE_ID'], date, date)
+
+exit if summary['total_grand'].nil?
+
 minutes = summary['total_grand'] / 1000 / 60
 
 client = Pixela::Client.new(username: ENV['PIXELA_USERNAME'], token: ENV['PIXELA_TOKEN'])
